290fd62fdfd6ca1de02a60fe21bd20c487867f41
Daniil Solonko <wwwsokol999@gmail.com>
2026-09-18 00:19:06 +0300

    Configure remote state and locking

--- providers.tf
+++ providers.tf
@@ -10,11 +10,21 @@ terraform {
   }
 
   required_version = "~>1.12.0"
-}
 
-provider "yandex" {
-  token     = var.token
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = var.default_zone
+  backend "s3" {
+    bucket = "terraform-state-wwwsokol999-05"
+    key    = "terraform.tfstate"
+    region = "ru-central1"
+
+    use_lockfile = true
+
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
 }
